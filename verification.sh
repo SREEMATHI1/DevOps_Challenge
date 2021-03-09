17f3385f919b6be81c320a2be83d64d7aa5646d3
SREEMATHI1 <sreesowmya1994@gmail.com>
2021-03-08 23:40:38 -0700

    fixed docker compose

---
+++ verification.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run Project
+docker-compose up --build -d
+sleep 5
+
+# Query results
+curl 127.0.0.1:5000/secret | jq '.secret_code'
+curl 127.0.0.1:5000/health | jq '.'
+
+
+sleep 100
+# Destroy 
+docker-compose down
